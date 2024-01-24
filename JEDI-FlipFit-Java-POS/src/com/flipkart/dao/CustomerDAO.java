211eca8ea095c3eef90cc1b54e9733d09446122d
shiworm <shivam82ap@gmail.com>
2024-01-24 10:29:18 +0530

    Updated DAO files

--- JEDI-FlipFit-Java-POS/src/com/flipkart/dao/CustomerDAO.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/dao/CustomerDAO.java
@@ -1,35 +1,25 @@
 package com.flipkart.dao;
 
-import java.util.List;
+import com.flipkart.bean.Customer;
 
-import com.flipkart.bean.GymOwner;
+public class CustomerDAO {
+    public boolean registerCustomer(String customerId){
 
-public class GymOwnerDAO {
-
-    // Create a new gym owner
-    public void createGymOwner(GymOwner gymOwner) {
-        // Implementation to create a new gym owner
+        return true;
     }
 
-    // Read gym owner details by ID
-    public GymOwner readGymOwnerById(int gymOwnerId) {
-        // Implementation to read gym owner details by ID
-        return null; // Replace with actual return statement
+    public Customer getCustomer(String customerId){
+
+        return new Customer();
     }
 
-    // Update gym owner details
-    public void updateGymOwner(GymOwner gymOwner) {
-        // Implementation to update gym owner details
+    public boolean updateCustomerDetails(String customerId){
+
+        return true;
     }
 
-    // Delete gym owner by ID
-    public void deleteGymOwnerById(int gymOwnerId) {
-        // Implementation to delete gym owner by ID
-    }
+    public boolean deleteCustomer(String customerId){
 
-    // Read all gym owners
-    public List<GymOwner> readAllGymOwners() {
-        // Implementation to read all gym owners
-        return null; // Replace with actual return statement
+        return true;
     }
 }
--- JEDI-FlipFit-Java-POS/src/com/flipkart/dao/SlotDAO.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/dao/SlotDAO.java
@@ -1,5 +1,25 @@
+
 package com.flipkart.dao;
 
+import com.flipkart.bean.Slot;
+
 public class SlotDAO {
-    //create, read, update, delete
+    public boolean createSlot() {
+        //Registers a new slot in the database
+        return false;
+    }
+
+    public Slot getSlotById(String slotId) {
+
+        return new Slot();
+    }
+
+    public boolean updateSlot(String slotId) {
+        return false;
+    }
+
+    public boolean deleteSlot(String slotId) {
+        return false;
+    }
+
 }
