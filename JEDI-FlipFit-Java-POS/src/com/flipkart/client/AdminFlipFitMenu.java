b9cdf7eee55b042a5172e6098d021a8db740e07b
shiworm <shivam82ap@gmail.com>
2024-01-24 12:45:13 +0530

    Added admin menu

--- JEDI-FlipFit-Java-POS/src/com/flipkart/client/AdminFlipFitMenu.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/client/AdminFlipFitMenu.java
@@ -1,4 +1,24 @@
 package com.flipkart.client;
 
+import java.util.Scanner;
+
 public class AdminFlipFitMenu {
+    public void showAdminFlipFitMenu(){
+        System.out.println("1. Approve Gym Onboarding Requests. \n2. Approve Gym Slot Update Requests.");
+
+        Scanner sc = new Scanner(System.in);
+        int action = sc.nextInt();
+
+        switch(action){
+            case 1:
+                System.out.println("Approving Onboarding Requests...");
+                break;
+
+            case 2:
+                System.out.println("Approving Slot Update Requests...");
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + action);        }
+    }
 }
--- JEDI-FlipFit-Java-POS/src/com/flipkart/client/FlipFitApplication.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/client/FlipFitApplication.java
@@ -12,20 +12,24 @@ public class FlipFitApplication {
         System.out.println("2. Registration of Gym Customer");
         System.out.println("3. Update Password");
         System.out.println("4. Exit");
-        Scanner sc=new Scanner(System.in);
-        int opt=sc.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+        int opt = sc.nextInt();
+
         String username;
         String passcode;
         int role;
+
         switch(opt)
         {
             case 1:
                 System.out.println("Enter Username : ");
-                username=sc.next();
+                username = sc.next();
                 System.out.println("Enter Passcode : ");
-                passcode=sc.next();
+                passcode = sc.next();
                 System.out.println("Enter role : \n1. Gym Customer\n2. Gym Owner\n3. GymFlipFit Admin");
                 role=sc.nextInt();
+
                 switch(role)
                 {
                     case(1):
@@ -35,15 +39,18 @@ public class FlipFitApplication {
                         System.out.println("Gym Owner Menu");
                         break;
                     case(3):
-                        System.out.println("Gym Admin Menu");
+                        System.out.println("Gym Admin Menu\n");
+                        AdminFlipFitMenu adminMenu = new AdminFlipFitMenu();
+                        adminMenu.showAdminFlipFitMenu();
                         break;
                     default:
                         throw new IllegalStateException("Unexpected value: " + role);
                 }
+
                 break;
             case 2:
             default:
-                throw new IllegalStateException("Unexpected value: " + opt);
+
         }
 
     }
