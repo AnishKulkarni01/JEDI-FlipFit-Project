aaf03b458a446be8fc7c52325bcaa9e538272ed0
mkoyna <157476869+mkoyna@users.noreply.github.com>
2024-01-24 23:37:06 +0530

    Update GymDAO.java

--- JEDI-FlipFit-Java-POS/src/com/flipkart/dao/GymDAO.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/dao/GymDAO.java
@@ -1,14 +1,51 @@
 package com.flipkart.dao;
 
+import com.flipkart.bean.Customer;
+import com.flipkart.bean.Gym;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static com.flipkart.dao.CustomerDAO.custDao;
+
 public class GymDAO {
 
-    public boolean onBoardGym(String gymId, String gymName, String gstin, String city, int seats){
-        //Onboards a new gym to our dashboard
-        return false;
+    static GymDAO gymdao=null;
+    List<Gym> gymList=new ArrayList<Gym>();
+    private int id = 0;
+
+    public static synchronized GymDAO getInstance()
+    {
+        if(gymdao==null)
+        {
+            gymdao=new GymDAO();
+        }
+        return gymdao;
     }
 
-    public boolean deleteGym(String gymId){
-        //Removes a gym from the database
-        return false;
+    public boolean onBoardGym(String gymName, String gstin, String city, int seats){
+        Gym gym=new Gym();
+        gym.setGymId(id++);
+        gym.setName(gymName);
+        gym.setGstin(gstin);
+        gym.setCity(city);
+        gym.setSeats(seats);
+        gymList.add(gym);
+
+        for(Gym gym1:gymList){
+            System.out.println(gym1.getName());
+        }
+
+        return true;
+    }
+
+    public boolean deleteGym(int gymId){
+        for (int i = 0; i < gymList.size(); i++) {
+            if(gymList.get(i).getGymId()==gymId){
+                gymList.remove(i);
+            }
+        }
+        return true;
     }
 }
