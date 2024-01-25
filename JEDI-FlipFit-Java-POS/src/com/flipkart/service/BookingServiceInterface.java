561b8ca9bea5505189b4c7db1e9daef93f2eca73
jainaashay123 <157475758+jainaashay123@users.noreply.github.com>
2024-01-24 22:53:11 -0800

    Delete JEDI-FlipFit-Java-POS directory

--- JEDI-FlipFit-Java-POS/src/com/flipkart/service/BookingServiceInterface.java
+++
@@ -1,4 +0,0 @@
-package com.flipkart.service;
-
-public interface BookingServiceInterface {
-}
--- JEDI-FlipFit-Java-POS/src/com/flipkart/service/SlotServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.flipkart.service;
-
-public interface SlotServiceInterface {
-    /**
-     * Adds a new slot
-     */
-    void addSlot();
-}
