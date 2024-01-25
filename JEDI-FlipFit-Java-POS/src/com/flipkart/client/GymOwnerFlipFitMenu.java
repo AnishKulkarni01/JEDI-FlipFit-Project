02ca6c94bc0c7d5e41c60397d6c859f92f208796
Rahul9414 <f20190718@pilani.bits-pilani.ac.in>
2024-01-25 10:23:07 +0530

    Added String Formatter for coloured menu

--- JEDI-FlipFit-Java-POS/src/com/flipkart/client/GymOwnerFlipFitMenu.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/client/GymOwnerFlipFitMenu.java
@@ -2,6 +2,8 @@ package com.flipkart.client;
 
 import java.util.Scanner;
 
+import static com.flipkart.constants.Constants.*;
+
 public class GymOwnerFlipFitMenu {
 
     public void showGymOwnerFlipMenu()
@@ -9,39 +11,33 @@ public class GymOwnerFlipFitMenu {
         int loopFlag=0;
         while(loopFlag==0)
         {
-        System.out.println("1. Request Gym Onboarding\n" +
-                "2. Gym Details Update\n" +
-                "3. Request Slot Update\n" +
-                "4. Edit Profile\n" +
-                "5. Log out\n" +
-                "6. Back");
+        System.out.println("1. " + YELLOW_COLOR + "Request Gym Onboarding\n" + RESET_COLOR + "2. " + YELLOW_COLOR + "Gym Details Update\n" + RESET_COLOR + "3. " + YELLOW_COLOR + "Request Slot Update\n" + RESET_COLOR + "4. " + YELLOW_COLOR + "Edit Profile\n" + RESET_COLOR + "5. " + YELLOW_COLOR +"Log out\n" + RESET_COLOR + "6. " + YELLOW_COLOR + "Back" + RESET_COLOR);
         Scanner sc=new Scanner(System.in);
         int opt=sc.nextInt();
 
             switch(opt)
             {
                 case 1:
-                    System.out.println("Request for Gym onboarding sent");
-
+                    System.out.println(BLUE_COLOR + "Request for Gym onboarding sent." + RESET_COLOR);
                     break;
                 case 2:
-                    System.out.println("Gym Details updated");
+                    System.out.println(GREEN_COLOR + "Gym Details updated." + RESET_COLOR);
                     break;
                 case 3:
-                    System.out.println("Request for Slot update sent");
+                    System.out.println(BLUE_COLOR + "Request for Slot update sent." + RESET_COLOR);
                     break;
                 case 4:
-                    System.out.println("Profile Edited");
+                    System.out.println(GREEN_COLOR + "Profile Edited successfully." + RESET_COLOR);
                     break;
                 case 5:
-                    System.out.println("Logging out");
+                    System.out.println(GREEN_COLOR + "Logged Out Successfully." + RESET_COLOR);
                     loopFlag=1;
                     break;
                 case 6:
                     loopFlag=1;
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + opt);
+                    throw new IllegalStateException(RED_COLOR + "Unexpected value: " + opt + RESET_COLOR);
 
             }
         }
--- JEDI-FlipFit-Java-POS/src/com/flipkart/constants/Constants.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/constants/Constants.java
@@ -1,4 +1,11 @@
 package com.flipkart.constants;
 
 public class Constants {
+    public static String GREEN_COLOR = "\u001B[32m";
+    public static String RED_COLOR = "\u001B[31m";
+    public static String RESET_COLOR = "\u001B[0m";
+    public static String YELLOW_COLOR = "\u001B[33m";
+    public static String BLUE_COLOR = "\u001B[34m";
+    public static String WHITE_COLOR = "\u001B[37m";
+    public static String BLACK_COLOR = "\u001B[30m";
 }
