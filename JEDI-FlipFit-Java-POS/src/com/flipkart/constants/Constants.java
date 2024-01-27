c7334e8a12ecea835c2c06971b7355cd3ea26028
shiworm <shivam82ap@gmail.com>
2024-01-27 15:06:27 +0530

    UserDAO changes incorporated

--- JEDI-FlipFit-Java-POS/src/com/flipkart/constants/Constants.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/constants/Constants.java
@@ -1,11 +1,15 @@
 package com.flipkart.constants;
 
 public class Constants {
-
+        //AUTHENTICATE
+        public static final String AUTHENTICATE_USER = "SELECT COUNT(userId) AS count FROM jedi_flipfit_schema.User WHERE username = ? AND password = ? AND role = ?;";
+        public static final String ADD_USER = "INSERT INTO jedi_flipfit_schema.User (username, password, role) VALUES (?, ?, ?);";
+        public static final String GET_USER = "SELECT * FROM jedi_flipfit_schema.User WHERE username = ?;";
+        public static final String UPDATE_USER_PASSWORD = "UPDATE jedi_flipfit_schema.User SET password = ? WHERE username = ?;";
 
         // ------------------------ GYM OWNER ------------------------
-        public static final String FETCH_ALL_GYM_OWNERS_QUERY = "SELECT * FROM jedi_flipfit_schema.GymOwner";
-        public static final String FETCH_ALL_PENDING_GYM_OWNERS_QUERY = "SELECT * FROM jedi_flipfit_schema.GymOwner where isApproved = 2";
+        public static final String FETCH_ALL_GYM_OWNERS_QUERY = "SELECT * FROM jedi_flipfit_schema.GymOwner;";
+        public static final String FETCH_ALL_PENDING_GYM_OWNERS_QUERY = "SELECT * FROM jedi_flipfit_schema.GymOwner where isApproved = 2;";
         public static final String SEND_GYM_OWNER_APPROVAL_REQ_QUERY = "UPDATE jedi_flipfit_schema.GymOwner SET isApproved = 2 WHERE Id =?;";
 
         public static final String ADD_GYM_CENTRE_QUERY = "INSERT INTO jedi_flipfit_schema.GymCentre (centreId, ownerId, centreName, gstin, city, capacity, price, isApproved) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
@@ -14,9 +18,9 @@ public class Constants {
 
 
         //  ------------ GYM OWNER ------------
-        public static final String REGISTER_GYM_OWNER = "Insert into GymOwner values (?,?,?,?,?,?,?)";
-        public static final String LOGIN_GYM_OWNER = "Select * from GymOwner where name=? and password=?";
-        public static final String SQL_APPROVE_GYM_OWNER_BY_ID_QUERY="Update GymOwner Set isApproved=? WHERE Id=?";
+        public static final String REGISTER_GYM_OWNER = "Insert into GymOwner values (?,?,?,?,?,?,?);";
+        public static final String LOGIN_GYM_OWNER = "Select * from GymOwner where name=? and password=?;";
+        public static final String SQL_APPROVE_GYM_OWNER_BY_ID_QUERY="Update GymOwner Set isApproved=? WHERE Id=?;";
         public static final String UPDATE_GYM_OWNER_DETAILS = "UPDATE jedi_flipfit_schema.GymOwner SET ? = ? WHERE gymOwnerId =?;";
         public static final String GET_GYM_OWNER_BY_ID = "SELECT * FROM jedi_flipfit_schema.GymOwner WHERE gymOwnerId = ?;";
         public static final String ADD_NEW_GYM_OWNER = "INSERT INTO jedi_flipfit_schema.GymOwner (username,password,email,contact) VALUES ( ?, ?, ?, ?);";
@@ -29,16 +33,16 @@ public class Constants {
 
 
         // ------------------------ GYM CENTRE ------------------------
-        public static final String FETCH_GYM_CENTRES_BY_OWNER_ID = "SELECT * FROM jedi_flipfit_schema.GymCentre where ownerId = ?";
-        public static final String FETCH_ALL_PENDING_GYM_REQUESTS = "SELECT * FROM jedi_flipfit_schema.Gym where isApproved = false";
-        public static final String FETCH_ALL_PENDING_GYM_REQUESTS_BY_GYMOWNERID = "SELECT * FROM jedi_flipfit_schema.Gym where gymOwnerId = ?";
+        public static final String FETCH_GYM_CENTRES_BY_OWNER_ID = "SELECT * FROM jedi_flipfit_schema.GymCentre where ownerId = ?;";
+        public static final String FETCH_ALL_PENDING_GYM_REQUESTS = "SELECT * FROM jedi_flipfit_schema.Gym where isApproved = false;";
+        public static final String FETCH_ALL_PENDING_GYM_REQUESTS_BY_GYMOWNERID = "SELECT * FROM jedi_flipfit_schema.Gym where gymOwnerId = ?;";
 
-        public static final String APPROVE_GYM_BY_ID="Update Gym Set isApproved=? WHERE gymId=?";
-        public static final String FETCH_GYM_CENTRES_BY_CITY = "SELECT * FROM jedi_flipfit_schema.GymCentre where city = ?";
+        public static final String APPROVE_GYM_BY_ID="Update Gym Set isApproved=? WHERE gymId=?;";
+        public static final String FETCH_GYM_CENTRES_BY_CITY = "SELECT * FROM jedi_flipfit_schema.GymCentre where city = ?;";
         public static final String SEND_GYM_ONBOARD_REQUEST = "INSERT INTO jedi_flipfit_schema.Gym (name,city,gstin,seats,gymOwnerId) VALUES ( ?, ?, ?, ?,?);";
         public static final String UPDATE_GYM_DETAILS = "UPDATE jedi_flipfit_schema.Gym SET ? = ? WHERE gymId =?;";
-        public static final String FETCH_ALL_AREAS = "SELECT DISTINCT city FROM jedi_flipfit_schema.Gym";
-        public static final String FETCH_FILTERED_GYMS = "SELECT * FROM jedi_flipfit_scehma.Gym WHERE ? = ?";
+        public static final String FETCH_ALL_AREAS = "SELECT DISTINCT city FROM jedi_flipfit_schema.Gym;";
+        public static final String FETCH_FILTERED_GYMS = "SELECT * FROM jedi_flipfit_scehma.Gym WHERE ? = ?;";
 
 
 //    public static final String SQL_APPROVE_GYM_CENTRE_BY_ID_QUERY = "Update GymCentre Set isApproved=? WHERE centreId=?";
@@ -54,7 +58,7 @@ public class Constants {
 
 
         //    ------- Customer ---------------
-        public static final String CUSTOMER_LOGIN_QUERY = "SELECT * FROM jedi_flipfit_schema.Customer WHERE username = ? AND password = ?";
+        public static final String CUSTOMER_LOGIN_QUERY = "SELECT * FROM jedi_flipfit_schema.Customer WHERE username = ? AND password = ?;";
         public static final String ADD_NEW_CUSTOMER = "INSERT INTO jedi_flipfit_schema.Customer (username,password,email,contact) VALUES ( ?, ?, ?, ?);";
         public static final String GET_CUSTOMER_BY_ID = "SELECT * FROM jedi_flipfit_schema.Customer WHERE customerId = ?;";
         public static final String GET_CUSTOMER_BY_USERNAME = "SELECT * FROM jedi_flipfit_schema.Customer WHERE username = ?;";
@@ -63,28 +67,28 @@ public class Constants {
 
 
         //  ----------- Booking -----------
-        public static final String GET_BOOKING_BY_CUSTOMER_ID ="Select * From jedi_flipfit_schema.Booking where customerId = ?";
-        public static final String CANCEL_BOOKING_BY_ID= "Delete from jedi_flipfit_schema.Booking where bookingId = ?";
-        public static final String ADD_BOOKING= "INSERT INTO jedi_flipfit_schema.Booking (customerID, slotId) values(?, ?)";
+        public static final String GET_BOOKING_BY_CUSTOMER_ID ="Select * From jedi_flipfit_schema.Booking where customerId = ?;";
+        public static final String CANCEL_BOOKING_BY_ID= "Delete from jedi_flipfit_schema.Booking where bookingId = ?;";
+        public static final String ADD_BOOKING= "INSERT INTO jedi_flipfit_schema.Booking (customerID, slotId) values(?, ?);";
 
         // ------------- Schedule -------------
-        public static final String ADD_SCHEDULE="INSERT INTO jedi_flipfit_schema.Schedule(scheduleId, date, slotId, availability ) values (?,?,?,?)";
-        public static final String GET_SCHEDULES_BY_DATE="SELECT * FROM jedi_flipfit_schema.Schedule WHERE date=?";
-        public static final String GET_SCHEDULE_BY_ID ="SELECT * FROM jedi_flipfit_schema.Schedule WHERE scheduleId=?";
-        public static final String MODIFY_SCHEDULE_AVAILABILITY ="UPDATE `flipfit`.`schedule` SET availability = ? WHERE (`scheduleId` = ?)";
+        public static final String ADD_SCHEDULE="INSERT INTO jedi_flipfit_schema.Schedule(scheduleId, date, slotId, availability ) values (?,?,?,?);";
+        public static final String GET_SCHEDULES_BY_DATE="SELECT * FROM jedi_flipfit_schema.Schedule WHERE date=?;";
+        public static final String GET_SCHEDULE_BY_ID ="SELECT * FROM jedi_flipfit_schema.Schedule WHERE scheduleId=?;";
+        public static final String MODIFY_SCHEDULE_AVAILABILITY ="UPDATE `flipfit`.`schedule` SET availability = ? WHERE (`scheduleId` = ?);";
 
         // ---------------- Slot ----------------
-        public static final String FETCH_ALL_SLOTS ="SELECT * FROM jedi_flipfit_schema.Slot";
-        public static final String FETCH_SLOT_BY_GYMID ="SELECT * FROM jedi_flipfit_schema.Slot WHERE gymId=?";
-        public static final String ADD_SLOT ="INSERT INTO jedi_flipfit_schema.Slot( gymId,date, startTime) values (?, ?, ?)";
-        public static final String FETCH_SLOT_BY_ID = "SELECT * FROM jedi_flipfit_schema.Slot WHERE slotId=?";
+        public static final String FETCH_ALL_SLOTS ="SELECT * FROM jedi_flipfit_schema.Slot;";
+        public static final String FETCH_SLOT_BY_GYMID ="SELECT * FROM jedi_flipfit_schema.Slot WHERE gymId=?;";
+        public static final String ADD_SLOT ="INSERT INTO jedi_flipfit_schema.Slot( gymId,date, startTime) values (?, ?, ?);";
+        public static final String FETCH_SLOT_BY_ID = "SELECT * FROM jedi_flipfit_schema.Slot WHERE slotId=?;";
         public static final String UPDATE_SLOT_DETAILS = "UPDATE jedi_flipfit_schema.Slot SET ? = ? WHERE slotId =?;";
 
-        public static final String FETCH_SLOT_BY_ID_AND_CENTRE_ID = "SELECT * FROM jedi_flipfit_schema.Slot WHERE slotId=? AND centreId=?";
+        public static final String FETCH_SLOT_BY_ID_AND_CENTRE_ID = "SELECT * FROM jedi_flipfit_schema.Slot WHERE slotId=? AND centreId=?;";
 
-        public static final String GET_BOOKING_BY_BOOKING_ID ="Select * From jedi_flipfit_schema.Booking where bookingId = ?";
+        public static final String GET_BOOKING_BY_BOOKING_ID ="Select * From jedi_flipfit_schema.Booking where bookingId = ?;";
 
 
-        public static final String GET_USERPLAN_BY_CUSTOMER_ID = "select * from slot join schedule where slot.slotId=schedule.slotId and schedule.scheduleId=?";
+        public static final String GET_USERPLAN_BY_CUSTOMER_ID = "select * from slot join schedule where slot.slotId=schedule.slotId and schedule.scheduleId=?;";
 
 }
--- JEDI-FlipFit-Java-POS/src/com/flipkart/dao/UserDAO.java
+++ JEDI-FlipFit-Java-POS/src/com/flipkart/dao/UserDAO.java
@@ -2,41 +2,104 @@ package com.flipkart.dao;
 
 import com.flipkart.bean.Customer;
 import com.flipkart.bean.User;
+import com.flipkart.utils.Utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
+
+import static com.flipkart.constants.Constants.*;
 
 public class UserDAO {
     static UserDAO userDao = null;
+    private Connection conn = null;
+    private PreparedStatement stmt = null;
+    private String currentUsername;
 
     public static synchronized UserDAO getInstance() {
         if(userDao == null) userDao = new UserDAO();
         return userDao;
     }
 
-    Map<String, List<String>> map = new HashMap<>();
-    private String currentUsername;
-
     public boolean check(String username,String password,String role){
-        return map.containsKey(username) && map.get(username).get(0).equals(password) && map.get(username).get(1).equals(role);
+        boolean isRegisteredUser = true;
+        try{
+            conn = Utils.connect();
+            stmt = conn.prepareStatement(AUTHENTICATE_USER);
+
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.setString(3, role);
+
+            ResultSet rs = stmt.executeQuery();
+            if(Integer.parseInt(rs.getString("count"))==0) isRegisteredUser = false;
+
+            stmt.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return isRegisteredUser;
     }
 
     public void addUser(String username,String password,String role) {
-        map.put(username, Arrays.asList(password, role));
+        try{
+            conn = Utils.connect();
+            stmt = conn.prepareStatement(ADD_USER);
+
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.setString(3, role);
+
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void setCurrentUser(String username){
         this.currentUsername = username;
     }
 
-    public List<String> getCurrentUser(){
+    public List<String> getCurrentUser() {
         if(currentUsername == null) {
             System.out.println("Please login to the system");
             return null;
+        } else{
+            List<String> userDetailList = new ArrayList<>();
+            try{
+                conn = Utils.connect();
+                stmt = conn.prepareStatement(GET_USER);
+
+                stmt.setString(1, this.currentUsername);
+
+                ResultSet rs = stmt.executeQuery();
+
+                userDetailList.add(rs.getString("username"));
+                userDetailList.add(rs.getString("role"));
+            } catch(Exception e){
+                e.printStackTrace();;
+            }
+
+            return userDetailList;
         }
-        return Arrays.asList(currentUsername, map.get(currentUsername).get(1));
     }
 
     public void updatePassword(String newPassword){
-        map.put(currentUsername, Arrays.asList(newPassword, map.get(currentUsername).get(1)));
+        try {
+            conn = Utils.connect();
+            stmt = conn.prepareStatement(UPDATE_USER_PASSWORD);
+
+            stmt.setString(1, newPassword);
+            stmt.setString(2, this.currentUsername);
+
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
