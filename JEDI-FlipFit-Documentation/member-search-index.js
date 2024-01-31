dae467143049b4a862f846b45d8046000b0d6795
Anish Kulkarni <anishkulkarni01@gmail.com>
2024-01-31 17:03:33 +0530

    Final push

---
+++ JEDI-FlipFit-Documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.flipkart.service","c":"GymServiceInterface","l":"modifyGymDetails(Gym, String, String, String, int)","url":"modifyGymDetails(com.flipkart.bean.Gym,java.lang.String,java.lang.String,java.lang.String,int)"}]
